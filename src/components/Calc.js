370817813b5cbd2d1bf83c5e8f755b07a1eccc20
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-02-09 03:56:43 +0200

    implement onclick for all buttons

--- src/components/Calc.js
+++ src/components/Calc.js
@@ -8,6 +8,11 @@ export default class Calc extends Component {
     this.state = { inputDisplay: 0 };
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  btnPressed = (e) => {
+    this.setState({ inputDisplay: e.target.innerHTML });
+  };
+
   render() {
     const { inputDisplay } = this.state;
     return (
@@ -17,62 +22,87 @@ export default class Calc extends Component {
             type="text"
             className="grid-item input"
             defaultValue={inputDisplay}
+            value={inputDisplay}
           />
           <button type="button" className="grid-item">
             AC
           </button>
-          <button type="button" className="grid-item">
+          <button type="button" className="grid-item" onClick={this.btnPressed}>
             +/-
           </button>
-          <button type="button" className="grid-item">
+          <button type="button" className="grid-item" onClick={this.btnPressed}>
             %
           </button>
-          <button type="button" className="grid-item orange">
+          <button
+            type="button"
+            className="grid-item orange"
+            onClick={this.btnPressed}
+          >
             &divide;
           </button>
-          <button type="button" className="grid-item">
+          <button type="button" className="grid-item" onClick={this.btnPressed}>
             7
           </button>
-          <button type="button" className="grid-item">
+          <button type="button" className="grid-item" onClick={this.btnPressed}>
             8
           </button>
-          <button type="button" className="grid-item">
+          <button type="button" className="grid-item" onClick={this.btnPressed}>
             9
           </button>
-          <button type="button" className="grid-item orange">
+          <button
+            type="button"
+            className="grid-item orange"
+            onClick={this.btnPressed}
+          >
             x
           </button>
-          <button type="button" className="grid-item">
+          <button type="button" className="grid-item" onClick={this.btnPressed}>
             4
           </button>
-          <button type="button" className="grid-item">
+          <button type="button" className="grid-item" onClick={this.btnPressed}>
             5
           </button>
-          <button type="button" className="grid-item">
+          <button type="button" className="grid-item" onClick={this.btnPressed}>
             6
           </button>
-          <button type="button" className="grid-item orange">
+          <button
+            type="button"
+            className="grid-item orange"
+            onClick={this.btnPressed}
+          >
             -
           </button>
-          <button type="button" className="grid-item">
+          <button type="button" className="grid-item" onClick={this.btnPressed}>
             1
           </button>
-          <button type="button" className="grid-item">
+          <button type="button" className="grid-item" onClick={this.btnPressed}>
             2
           </button>
-          <button type="button" className="grid-item">
+          <button type="button" className="grid-item" onClick={this.btnPressed}>
             3
           </button>
-          <button type="button" className="grid-item orange">
+          <button
+            type="button"
+            className="grid-item orange"
+            onClick={this.btnPressed}
+          >
             +
           </button>
-          <button type="button" className="grid-item zero">
+          <button
+            type="button"
+            className="grid-item zero"
+            onClick={this.btnPressed}
+          >
             0
           </button>
-          <button type="button" className="grid-item">
+          <button type="button" className="grid-item" onClick={this.btnPressed}>
             .
           </button>
-          <button type="button" className="grid-item orange">
+          <button
+            type="button"
+            className="grid-item orange"
+            onClick={this.btnPressed}
+          >
             =
           </button>
         </div>
