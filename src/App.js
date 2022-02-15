dd8265d6a57c6b5cd09892ed9cab716a12b28873
ahmed tawfik <a.alrakhawy@gmail.com>
2022-02-15 14:25:54 +0200

    Revert "Implement routing"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
 import Calc from './components/Calc';
-import Header from './components/Header';
-import Home from './components/Home';
-import Quote from './components/Quote';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/calc" exact element={<Calc />} />
-        <Route path="/quote" exact element={<Quote />} />
-      </Routes>
+      <Calc />
     </div>
   );
 }
